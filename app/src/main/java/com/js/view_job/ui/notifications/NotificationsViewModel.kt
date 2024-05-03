3f4a24f3bb6b9fc24319eddc4a53207ef2536b3b
cheonjoosung <cjswntjd@gmail.com>
2024-05-03 21:11:48 +0900

    git init

---
+++ app/src/main/java/com/js/view_job/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.js.view_job.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
