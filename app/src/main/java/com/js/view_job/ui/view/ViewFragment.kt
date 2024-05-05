091fbbc51a9f1469b99db05e3a6469d6b7bbd2c9
cheonjoosung <cjswntjd@gmail.com>
2024-05-05 09:57:43 +0900

    뷰 탭 - webView 로드 추가

--- app/src/main/java/com/js/view_job/ui/view/ViewFragment.kt
+++ app/src/main/java/com/js/view_job/ui/view/ViewFragment.kt
@@ -1,10 +1,13 @@
 package com.js.view_job.ui.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.js.view_job.databinding.FragmentViewBinding
@@ -14,7 +17,7 @@ class ViewFragment : Fragment() {
     private var _binding: FragmentViewBinding? = null
     private val binding get() = _binding!!
 
-    val args: ViewFragmentArgs by navArgs()
+    private val args: ViewFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,17 +29,34 @@ class ViewFragment : Fragment() {
 
         _binding = FragmentViewBinding.inflate(inflater, container, false)
 
-        initWebView()
+        initWebView(companyUrl)
 
         initFloatingView()
 
         return binding.root
     }
 
-    private fun initWebView() {
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView(companyUrl: String ) {
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
 
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(true)
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+        }
+
+        if (companyUrl.isEmpty()) {
+            binding.webView.loadUrl("https://www.samsungcareers.com/hr/")
+        } else {
+            binding.webView.loadUrl(companyUrl)
+        }
     }
 
+    // TODO ViewModel & repository 추가, DB에 있는 목록 보여주기
     private fun initFloatingView() {
 
     }
